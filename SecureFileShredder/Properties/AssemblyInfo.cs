71e99eb45a358cab7b52ecb6095e6e576556cad3
Sayed Muhammad Idrees <sayedidrees47@gmail.com>
2024-10-11 10:53:11 +0400

    setup installer created

---
+++ SecureFileShredder/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// In SDK-style projects such as this one, several assembly attributes that were historically
+// defined in this file are now automatically added during build and populated with
+// values defined in project properties. For details of which attributes are included
+// and how to customise this process see: https://aka.ms/assembly-info-properties
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("aa5676d5-3046-464e-a7d1-f90e10c706ff")]  
+[assembly: AssemblyDescription("This software is design to delete the secure files in efficient way so it cannot be recoverable.")] 
+[assembly: AssemblyCopyright("Copyright © Softasium 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
